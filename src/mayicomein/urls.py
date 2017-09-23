5ff37526b96bc1ce7b89cebf70e0aa08fac272d2
Sandeep Sihari <kumarsandeep91@gmail.com>
2017-09-23 23:44:35 +0530

    static files served properly...

--- src/mayicomein/urls.py
+++ src/mayicomein/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from rest_framework import routers
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 router = routers.DefaultRouter()
@@ -31,4 +32,4 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
